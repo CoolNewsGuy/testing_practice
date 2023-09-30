890552bf3987044fb3a50b0260b12a7d8f33d1d6
Aymen <joewilliamdd31@gmail.com>
2023-09-30 08:21:01 +0100

    Test reverseString with more inputs

--- tests/reverseString.test.ts
+++ tests/reverseString.test.ts
@@ -1,6 +1,6 @@
 it("Reverse the string", () => {
-    const words = ["foo", "BAR", "Foo BAr", "Pizza", "1234"];
-    const results = ["oof", "RAB", "rAB ooF", "azziP", "4321"];
+    const words = ["foo", "BAR", "Foo BAr", "Pizza", "1234", "", " "];
+    const results = ["oof", "RAB", "rAB ooF", "azziP", "4321", "", " "];
 
     for (const i in words) {
         expect(reverseString(words[i])).toBe(results[i]);
