53a0afbf161fe39390006af91666e85702317dbe
Aymen <joewilliamdd31@gmail.com>
2023-09-30 19:47:39 +0100

    Test against UPPERCASE letters

--- tests/caesarCipher.test.ts
+++ tests/caesarCipher.test.ts
@@ -7,8 +7,29 @@ describe("Caesar Cipher", () => {
         };
 
         for (const i in words) {
-            expect(caesarCipher(words[i], 1)).toBe(results.forShiftFactorOne[i])
-            expect(caesarCipher(words[i], 2)).toBe(results.forShiftFactorTwo[i])
+            expect(caesarCipher(words[i], 1)).toBe(
+                results.forShiftFactorOne[i]
+            );
+            expect(caesarCipher(words[i], 2)).toBe(
+                results.forShiftFactorTwo[i]
+            );
+        }
+    });
+
+    it("shifts UPPERCASE letters correctly", () => {
+        const words = ["FOO", "BAR", "PIZZA", "SUPERMAN"];
+        const results = {
+            forShiftFactorOne: ["GPP", "CBS", "QJAAB", "TVQFSNBO"],
+            forShiftFactorTwo: ["HQQ", "DCT", "RKBBC", "UWRGTOCP"],
+        };
+
+        for (const i in words) {
+            expect(caesarCipher(words[i], 1)).toBe(
+                results.forShiftFactorOne[i]
+            );
+            expect(caesarCipher(words[i], 2)).toBe(
+                results.forShiftFactorTwo[i]
+            );
         }
     });
 });
