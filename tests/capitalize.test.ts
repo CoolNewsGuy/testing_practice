937feec0efd8e9b8945f58783ae7409d45ea9a2b
Aymen <joewilliamdd31@gmail.com>
2023-09-30 08:13:20 +0100

    Test capitalize with more inputs

--- tests/capitalize.test.ts
+++ tests/capitalize.test.ts
@@ -1,8 +1,8 @@
 import { capitalize } from "../src/capitalize";
 
 it("Capitalizes the string", () => {
-    const words = ["foo", "bAr", "pizza", "BaNANa"];
-    const results = ["Foo", "BAr", "Pizza", "BaNANa"];
+    const words = ["foo", "bAr", "pizza", "BaNANa", "2022", "-2002", "", " "];
+    const results = ["Foo", "BAr", "Pizza", "BaNANa", "2022", "-2002", "", " "];
 
     for (const i in words) {
         expect(capitalize(words[i])).toBe(results[i]);
