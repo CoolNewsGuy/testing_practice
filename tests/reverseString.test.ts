28414c3cfcae477432db45996e9883c0ec8b736d
Aymen <joewilliamdd31@gmail.com>
2023-09-30 07:52:47 +0100

    Write test for reverseString function

---
+++ tests/reverseString.test.ts
@@ -0,0 +1,8 @@
+it("Reverse the string", () => {
+    const words = ["foo", "BAR", "Foo BAr", "Pizza", "1234"];
+    const results = ["oof", "RAB", "rAB ooF", "azziP", "4321"];
+
+    for (const i in words) {
+        expect(reverseString(words[i])).toBe(results[i]);
+    }
+});
