88226eeee873f85f600dacdf9d6a815f552b3843
Aymen <joewilliamdd31@gmail.com>
2023-09-30 19:44:55 +0100

    Test against LOWERCASE letters

--- tests/caesarCipher.test.ts
+++ tests/caesarCipher.test.ts
@@ -1,1 +1,14 @@
-describe("Caesar Cipher", () => {});
+describe("Caesar Cipher", () => {
+    it("shifts LOWERCASE letters correctly", () => {
+        const words = ["foo", "bar", "pizza", "superman"];
+        const results = {
+            forShiftFactorOne: ["gpp", "cbs", "qjaab", "tvqfsnbo"],
+            forShiftFactorTwo: ["hqq", "dct", "rkbbc", "uwrgtocp"],
+        };
+
+        for (const i in words) {
+            expect(caesarCipher(words[i], 1)).toBe(results.forShiftFactorOne[i])
+            expect(caesarCipher(words[i], 2)).toBe(results.forShiftFactorTwo[i])
+        }
+    });
+});
