241d3a3d91b820032c9cdac716c845099a481809
Aymen <joewilliamdd31@gmail.com>
2023-10-01 08:36:35 +0100

    Create caesarCipher function

--- src/caesarCipher.ts
+++ src/caesarCipher.ts
@@ -4,6 +4,33 @@ const LOWER_Z_CHAR_CODE = 122;
 const UPPER_A_CHAR_CODE = 65;
 const UPPER_Z_CHAR_CODE = 90;
 
+export function caesarCipher(str: string, shiftFactor: number): string {
+    while (shiftFactor < 0) {
+        shiftFactor += 26;
+    }
+
+    shiftFactor %= 26;
+
+    for (let i = 0; i < str.length; i++) {
+        const currentChar = str[i];
+
+        if (isAlpha(currentChar)) {
+            let newCharCode: number = str.charCodeAt(i) + shiftFactor;
+
+            if (!isAlpha(String.fromCharCode(newCharCode))) {
+                newCharCode -= 26;
+            }
+
+            str =
+                str.slice(0, i) +
+                String.fromCharCode(newCharCode) +
+                str.slice(i + 1);
+        }
+    }
+
+    return str;
+}
+
 function isAlpha(char: string): boolean {
     if (char.length === 0) {
         throw new Error(
